0f89bd5bc1b43f2239652b3d36ec1a84e42de63e
Mark Bond <markbond615@gmail.com>
2025-05-06 10:14:59 -0500

    Added display of non-staff users

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css"
+import { CustomersList } from "./components/customers/CustomersList";
 import { TicketList } from "./components/tickets/TicketList";
 
 export const App = () => {
   return <>
-    <TicketList/>
+    <TicketList />
+    <CustomersList />
   </>;
 }
